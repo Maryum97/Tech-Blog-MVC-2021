0807514df3bd2bcbbbdc0f12cae2dd892e420bcc
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-27 23:49:02 +0000

    edited code for dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -37,37 +37,6 @@ router.get('/post/:id', withAuth, async (req, res) => {
     }
 })
 
-// Render the dashboard page, only for a logged in user
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.findAll(req.session.user_id, {
-            include: [
-                {
-                    model: Comment,
-                    attributes: ['id', 'text', 'post_id', 'user_id'],
-                    include: {
-                        model: User,
-                        attributes: ['name']
-                    }
-                },
-                {
-                    model: User,
-                    attributes: ['name']
-                }
-            ]
-        })
-
-        // serialize data before passing to template
-        const posts = postData.map(post => post.get({ plain: true }));
-        res.render('dashboard', { posts, loggedIn: true });
-    }
-
-    catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-})
-
 // Edit a post
 router.get('/edit/:id', withAuth, async (req, res) => {
     try {
