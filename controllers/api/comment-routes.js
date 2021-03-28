452cceb4a18bf1678db2909ed4fc0370d5de2264
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-28 17:24:28 +0100

    edited code for comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,6 +3,22 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Get comments
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll();
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No post found with this id' });
+        }
+    }
+
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
 // Create comment
 router.post('/', withAuth, async (req, res) => {
     try {
@@ -10,6 +26,7 @@ router.post('/', withAuth, async (req, res) => {
             // ...req.body,
             text: req.body.text,
             user_id: req.session.user_id,
+            post_id: req.body.post_id
         })
 
         res.status(200).json(commentData);
